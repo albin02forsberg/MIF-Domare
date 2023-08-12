463d0fb3508142aded326efe7c9ae65c324268c4
Albin Forsberg <albin02forsberg@gmail.com>
2023-08-12 23:50:17 +0200

    Updated theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 "use client";
-import { NavigationMenu } from "@/components/ui/navigation-menu";
 import * as React from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Navbar } from "@/components/layout/navbar";
 import firebase_app from "@/lib/firebase";
 import { getAuth } from "firebase/auth";
-import AuthenticationPage from "@/components/layout/auth/login";
 import { AuthContextProvider } from "@/components/context/AuthContext";
-import { Button } from "primereact/button";
 import { Panel } from "primereact/panel";
 
 //theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+//import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/soho-light/theme.css"
 
-//core
 import "primereact/resources/primereact.min.css";
 import 'primeicons/primeicons.css';
 
@@ -26,6 +23,7 @@ import {
 const auth = getAuth(firebase_app);
 
 const inter = Inter({ subsets: ["latin"] });
+
 
 export default function RootLayout({
   children,
